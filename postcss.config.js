f891ed0fb8648a91b71b6856f24b2f7955ba3973
kazk <kazk.dev@gmail.com>
2021-05-04 15:20:17 -0700

    Update dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     require("tailwindcss"),
     require("autoprefixer"),
-    require("postcss-nesting"),
+    require("postcss-nested"),
     ...(process.env.NODE_ENV === "production" ? [require("cssnano")] : []),
   ],
 };
